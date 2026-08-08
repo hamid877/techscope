0532a7398807a92bbebc0d8e835ac06e89ad315c
Mohd Hamid <hamidmohd726@gmail.com>
2026-08-08 18:34:57 +0530

    chore: establish project foundation

---
+++ src/domain/constants/score.constants.ts
@@ -0,0 +1,6 @@
+export const SCORE_MIN = 0;
+export const SCORE_MAX = 100;
+export const CONFIDENCE_MIN = 0;
+export const CONFIDENCE_MAX = 100;
+export const COMPLETENESS_MIN = 0;
+export const COMPLETENESS_MAX = 100;
